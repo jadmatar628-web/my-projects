64080c2e24c66835f0f04346dc70c4059c86346a
jadmatar628-web <jadmatar628@gmail.com>
2025-12-02 12:54:38 +0200

    Sync

--- WEB_DEVELOPEMENT/UNI_DEMO/script.js
+++ WEB_DEVELOPEMENT/UNI_DEMO/script.js
@@ -1,1 +1,59 @@
-let role= document.getElementById("role")
+    document.getElementById("btn").addEventListener("click",signupval);
+    document.getElementById("btne").addEventListener("click", loginval);
+
+    function emailval(email)
+    {
+        if(email.length===0)
+        {
+            alert("Email cannot be empty");
+            return false;
+        }
+        if(!email.includes("@") || !email.includes("."))
+        {
+            alert("Invalid E-mail Format");
+            return false;
+        }
+        return true;
+    }
+
+    function passval(p,pp)
+    {
+        if(p.length===0)
+        {
+            alert("Password cannot be empty");
+            return false;
+        }
+            if(pp.length===0)
+        {
+            alert("Password confirmation cannot be empty");
+            return false;
+        }
+            if(p.length<8)
+        {
+            alert("Passwords must contains at least 8 characters.");
+            return false;
+        }
+        if(p!=pp)
+        {
+            alert("Passwords do not match");
+            return false;
+        }
+        return true;
+    }
+    function signupval(){
+            let mail=document.getElementById("email").value;
+            if(!emailval(mail)) return;
+        let pass1=document.getElementById("pwd").value;
+        let pass2=document.getElementById("pwd2").value;
+                    if (!passval(pass1, pass2)) return;
+        alert("Success!");
+    }
+
+    function loginval()
+    {
+                let mail=document.getElementById("emaile").value;
+            emailval(mail);
+            let pas=document.getElementById("pwde").value;
+             if (!emailval(mail)) return;
+            if (!passval(pass, pass)) return;
+    }
