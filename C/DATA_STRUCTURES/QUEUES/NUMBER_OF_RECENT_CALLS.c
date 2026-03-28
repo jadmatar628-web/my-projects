2f63684f3e3bb7baafd56d35ee0b2f2d44dea5b6
jadmatar628-web <jadmatar628@gmail.com>
2026-03-28 16:01:36 +0200

    quick commit 2026-03-28 16:01:35

--- C/DATA_STRUCTURES/QUEUES/NUMBER_OF_RECENT_CALLS.c
+++ C/DATA_STRUCTURES/QUEUES/NUMBER_OF_RECENT_CALLS.c
@@ -1,29 +1,22 @@
 #include"queue.h"
 #include<stdlib.h>
-Queue *define()
+
+Queue * create(int capacity)
 {
     Queue *q=malloc(sizeof(Queue));
-    q->size=0;
+    q->arr=malloc(sizeof(capacity));
+    q->capacity=capacity;
     q->front=0;
     q->rear=-1;
-    q->capacity=10000;
-    q->arr=malloc(q->capacity*sizeof(int));
     return q;
 }
-int ping (int t,Queue *q)
+
+int NumberOfRecenetCalls(Queue *q, int t)
 {
-    if(!(isFull(q)))
-    {
-        enqueue(q,t);
-    }
-    else
-    {
-        return 1;
-    }
-    int threshold=t-3000;
-    while(!(isEmpty(q)) && q->arr[q->front]<threshold)
+    enqueue(q,t);
+    while(!isEmpty(q) && q->arr[q->front]<t-3000)
     {
         dequeue(q);
     }
-    return q->size;
+    return q->size; 
 }
--- C/DATA_STRUCTURES/QUEUES/queue.c
+++ C/DATA_STRUCTURES/QUEUES/queue.c
@@ -1,37 +1,69 @@
-#include"queue.h"
+#include "queue.h"
+#include <stdlib.h>
 
 int isEmpty(Queue *q)
 {
-    return (q->size==0);
+    return q->size == 0;
 }
 
 int isFull(Queue *q)
 {
-    return (q->capacity==q->size);
+    return q->size == q->capacity;
+}
+
+void resizeQueue(Queue *q)
+{
+    int newCapacity = q->capacity * 2;
+    int *newArr = malloc(newCapacity * sizeof(int));
+
+    int j = 0;
+    for (int i = 0; i < q->size; i++)
+    {
+        newArr[i] = q->arr[(q->front + i) % q->capacity];
+    }
+
+    free(q->arr);
+    q->arr = newArr;
+    q->capacity = newCapacity;
+    q->front = 0;
+    q->rear = q->size - 1;
 }
 
 void enqueue(Queue *q, int value)
 {
-    if(isFull(q))
+    if (isFull(q))
     {
-        return ;
+        resizeQueue(q);
     }
-    q->rear=(q->rear +1) % q->capacity;
-    q->arr[q->rear]=value;
+
+    q->rear = (q->rear + 1) % q->capacity;
+    q->arr[q->rear] = value;
     q->size++;
 }
+
 void dequeue(Queue *q)
 {
-    if(isEmpty(q))
+    if (isEmpty(q))
     {
-        return ;
+        return;
     }
-    q->front=(q->front +1)%q->capacity;
+
+    q->front = (q->front + 1) % q->capacity;
     q->size--;
+
+    if (q->size == 0)
+    {
+        q->front = 0;
+        q->rear = -1;
+    }
 }
+
 int peek(Queue *q)
 {
-    if(isEmpty(q))
-    return -1;
+    if (isEmpty(q))
+    {
+        return -1;
+    }
+
     return q->arr[q->front];
 }
