d3be132dbf6ce3a67eeea94b61feacca0bb753bd
jadmatar628-web <jadmatar628@gmail.com>
2026-03-29 16:14:59 +0300

    quick commit 2026-03-29 16:14:59

--- C/DATA_STRUCTURES/STACKS/remove_k_digits.c
+++ C/DATA_STRUCTURES/STACKS/remove_k_digits.c
@@ -1,2 +1,23 @@
-#include"STACK.H"
+#include<STACK_A.H>
 
+Stack * removeKdigits(int k, int *arr, int size, Stack *s)
+{
+    for(int i=0;i<size;i++)
+    {
+        int current=arr[i];
+        while(k>0 && !isEmpty(s) && current<s->arr[s->top])
+        {
+                pop(s);
+                k--;
+        }
+        push(s,current);
+    }
+    if(k>0)
+    {
+        for(k;k>0;k--)
+        {
+            pop(s);
+        }
+    }
+    return s;
+}
