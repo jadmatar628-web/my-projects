eafe36a8a7f2a7dc1eebb1c5d3208dce2c237b8b
jadmatar628-web <jadmatar628@gmail.com>
2026-04-05 10:58:22 +0300

    quick commit 2026-04-05 10:58:22

---
+++ C/LEETCODE/home/jadmatar/MY_PROJECTS/C/LEETCODE/1823_Find the Winner of the Circular Game.c
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=1823 lang=c
+ *
+ * [1823] Find the Winner of the Circular Game
+ */
+
+// @lc code=start
+int findTheWinner(int n, int k) {
+    
+}
+// @lc code=end
+
---
+++ C/LEETCODE/home/jadmatar/MY_PROJECTS/C/LEETCODE/950_Reveal Cards In Increasing Order.c
@@ -0,0 +1,120 @@
+/*
+ * @lc app=leetcode id=950 lang=c
+ *
+ * [950] Reveal Cards In Increasing Order
+ */
+
+// @lc code=start
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+typedef struct Queue
+{
+    int *arr;
+    int front;
+    int rear;
+    int size;
+    int capacity;
+}Queue;
+int isEmpty(Queue *q)
+{
+    return q->size == 0;
+}
+
+int isFull(Queue *q)
+{
+    return q->size == q->capacity;
+}
+
+void resizeQueue(Queue *q)
+{
+    int newCapacity = q->capacity * 2;
+    int *newArr = malloc(newCapacity * sizeof(int));
+
+    int j = 0;
+    for (int i = 0; i < q->size; i++)
+    {
+        newArr[i] = q->arr[(q->front + i) % q->capacity];
+    }
+
+    free(q->arr);
+    q->arr = newArr;
+    q->capacity = newCapacity;
+    q->front = 0;
+    q->rear = q->size - 1;
+}
+
+void enqueue(Queue *q, int value)
+{
+    if (isFull(q))
+    {
+        resizeQueue(q);
+    }
+
+    q->rear = (q->rear + 1) % q->capacity;
+    q->arr[q->rear] = value;
+    q->size++;
+}
+
+void dequeue(Queue *q)
+{
+    if (isEmpty(q))
+    {
+        return;
+    }
+
+    q->front = (q->front + 1) % q->capacity;
+    q->size--;
+
+    if (q->size == 0)
+    {
+        q->front = 0;
+        q->rear = -1;
+    }
+}
+
+int peek(Queue *q)
+{
+    if (isEmpty(q))
+    {
+        return -1;
+    }
+
+    return q->arr[q->front];
+}
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+#include<stdlib.h>
+int* deckRevealedIncreasing(int* deck, int deckSize, int* returnSize) 
+{
+    qsort(deck,deckSize,sizeof(int),compare);
+Queue *queue=malloc(sizeof(Queue));
+queue->size=deckSize;
+queue->front=0;
+queue->rear=-1;
+queue->capacity=deckSize;
+queue->arr = malloc(deckSize * sizeof(int));
+for(int i=0;i<deckSize;i++)
+{
+    queue->arr[i]=i;
+}
+int* Answer = malloc(deckSize * sizeof(int));
+for(int i=0;i<deckSize;i++)
+{
+    Answer[peek(queue)]=deck[i];
+    int valueToMove = peek(queue);
+    dequeue(queue);
+    enqueue(queue, valueToMove);
+    dequeue(queue);
+    if(i==deckSize-1)
+    {
+        Answer[i]=deck[queue->arr[0]];
+        break;
+    }
+}
+*returnSize = deckSize;
+return Answer;
+}
+// @lc code=end
+
