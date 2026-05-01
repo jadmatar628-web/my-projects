cf4669ff334fb0b1a25afbdaf3c4fc4817663569
jadmatar628-web <jadmatar628@gmail.com>
2026-05-01 14:07:31 +0300

    quick commit 2026-05-01 14:07:31

---
+++ C/Sorting_Algorithms/merging_sort.c
@@ -0,0 +1,58 @@
+void merge(int arr[], int left, int mid, int right)
+{
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+
+    // copy data
+    for(int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+
+    for(int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    // merge
+    while(i < n1 && j < n2)
+    {
+        if(L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // remaining
+    while(i < n1)
+    {
+        arr[k] = L[i];
+        i++; k++;
+    }
+
+    while(j < n2)
+    {
+        arr[k] = R[j];
+        j++; k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right)
+{
+    if(left >= right)
+        return;
+
+    int mid = (left + right) / 2;
+
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+
+    merge(arr, left, mid, right);
+}
